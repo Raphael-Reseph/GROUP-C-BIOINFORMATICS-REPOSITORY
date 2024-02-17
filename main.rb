c55e8facec08ac56795bf8430c7b58857521e075
Isaacboda <160308516+Isaacboda@users.noreply.github.com>
2024-02-17 14:22:48 +0100

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,13 @@
+=begin
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+=end
+puts "my information"
+puts "name:isaacjames"
+puts "email:isacboda23@gmail.com"
+puts "user:isaac james"
+puts "interest:biotechnology"
